5119d71fdf7ca11620a904a88ffd39f7759375fd
George Roberts <george.e.roberts@gmail.com>
2019-04-29 09:05:26 +0100

    add semi-functional dynamic t-shirt select drop down options

--- js/app.js
+++ js/app.js
@@ -25,3 +25,38 @@ $('#title').on('change', function() {
   } else $('#other-title').hide();
 });
 
+
+/* ===========================================
+T-shirts section: show only colors that correspond to selected Design theme 
+=============================*/
+
+// hide all color options on page load and show only message "Please select a T-Shirt theme"
+$('#color option').hide();
+
+// listen for change on #design
+  // if value = "Select Theme", hide all options
+  // if value = "js puns", hide I love JS options
+  // else if value = I love JS, hide JS puns options
+$('#design').on('change', function() {
+  
+  switch (  $('#design').val()  ) {
+    case "Select Theme":
+      $('#color option').hide(); 
+      $('option[value="Please select a T-shirt theme"]').show(); // is this line working?
+      break;
+
+    case "js puns":
+      $('#color option').hide();
+      $('option[value="cornflowerblue"]').show();
+      $('option[value="darkslategrey"]').show();
+      $('option[value="gold"]').show();
+      break;
+
+    case "heart js":
+      $('#color option').hide();  
+      $('option[value="tomato"]').show();
+      $('option[value="steelblue"]').show();
+      $('option[value="dimgrey"]').show();
+      break;
+  } 
+});
